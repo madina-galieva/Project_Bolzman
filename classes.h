7c946fb68514f9cddc2dcbad4d4923b97e52046a
madina-galieva <38853556+madina-galieva@users.noreply.github.com>
2018-05-11 08:49:25 +0300

    Just returned show

--- classes.h
+++ classes.h
@@ -17,7 +17,7 @@ protected:
 public:
     Wall (int l, int a) { length = l; angle = a; } //конструктор
 public slots:
-    void Print_Walls();
+  //  void Print_Walls();
     void show (int ix,int iy); // нарисовать стенку
 signals:
 
@@ -35,9 +35,9 @@ public:
     Barrier (int icx, int icy, int d)
        {centre_x = icx; centre_y = icy; hole_radius = d;}
 public slots:
-    void Print_Barrier(int icx, int icy, int d);
+ //   void Print_Barrier(int icx, int icy, int d);
  // void hide (int ix,int iy); //скрыть перегородку
- // void show (int ix,int iy); //нарисовать  перегородку
+    void show (int ix,int iy); //нарисовать  перегородку
     int hole_scale (int dy);  // изменение радиуса отверстия в перегородке
  // void move (int dx); // поменять положение перегородки
     int getcentre_x()  { return centre_x; }
@@ -59,15 +59,15 @@ protected:
 public:
     Ball() {;}
 public slots:
-    void Print_Atom(int centreX,int centreY, int radius);
+ // void Print_Atom(int centreX,int centreY, int radius);
     void Print_Point(int centreX, int centreY);
-  //  void show();
-  //  void hide();
-  //  int move();
-  // int hit (Ball b2); //столкновение с другой молекулой
-  //  int hit_on_wall (); //столкновение со стенкой
-  //  void hit_on_barr (int x, int y, int d); // столкновение с перегородкой
-  /*  void set (int m, int value) //функция инициализирующая поля класса (в зависимости от значения m)
+    void show();
+//  void hide();
+//  int move();
+//  int hit (Ball b2); //столкновение с другой молекулой
+//  int hit_on_wall (); //столкновение со стенкой
+//  void hit_on_barr (int x, int y, int d); // столкновение с перегородкой
+/*  void set (int m, int value) //функция инициализирующая поля класса (в зависимости от значения m)
      {
      if(m==1) centreX = value;
      if(m==2) centreY = value;
