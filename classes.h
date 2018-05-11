7ba0f89e8c45b23d4a9e478027640a23d4950bdb
madina-galieva <38853556+madina-galieva@users.noreply.github.com>
2018-05-11 08:45:10 +0300

    First unnecessary updates 9am Friday
    I've hidden some functions that I really don't know how to write at the moment; 
    other functions I am going to write in classes.cpp (their bodies, I mean)
    And I used name Print_(smth) instead of show, that's probably should be the same thing (maybe I'll return the name later)

--- classes.h
+++ classes.h
@@ -17,6 +17,7 @@ protected:
 public:
     Wall (int l, int a) { length = l; angle = a; } //конструктор
 public slots:
+    void Print_Walls();
     void show (int ix,int iy); // нарисовать стенку
 signals:
 
@@ -34,10 +35,11 @@ public:
     Barrier (int icx, int icy, int d)
        {centre_x = icx; centre_y = icy; hole_radius = d;}
 public slots:
-    void hide (int ix,int iy); //скрыть перегородку
-    void show (int ix,int iy); //нарисовать  перегородку
+    void Print_Barrier(int icx, int icy, int d);
+ // void hide (int ix,int iy); //скрыть перегородку
+ // void show (int ix,int iy); //нарисовать  перегородку
     int hole_scale (int dy);  // изменение радиуса отверстия в перегородке
-    void move (int dx); // поменять положение перегородки
+ // void move (int dx); // поменять положение перегородки
     int getcentre_x()  { return centre_x; }
     int getcentre_y()  { return centre_y; }
     int gethole_radius() { return hole_radius; }
@@ -57,13 +59,15 @@ protected:
 public:
     Ball() {;}
 public slots:
-    void show();
-    void hide();
-    int move();
-    int hit (Ball b2); //столкновение с другой молекулой
-    int hit_on_wall (); //столкновение со стенкой
-    void hit_on_barr (int x, int y, int d); // столкновение с перегородкой
-    void set (int m, int value) //функция инициализирующая поля класса (в зависимости от значения m)
+    void Print_Atom(int centreX,int centreY, int radius);
+    void Print_Point(int centreX, int centreY);
+  //  void show();
+  //  void hide();
+  //  int move();
+  // int hit (Ball b2); //столкновение с другой молекулой
+  //  int hit_on_wall (); //столкновение со стенкой
+  //  void hit_on_barr (int x, int y, int d); // столкновение с перегородкой
+  /*  void set (int m, int value) //функция инициализирующая поля класса (в зависимости от значения m)
      {
      if(m==1) centreX = value;
      if(m==2) centreY = value;
@@ -71,5 +75,5 @@ public slots:
      if(m==4) angl = 1.0*value;
      }
 signals:
-
+*/
 };
